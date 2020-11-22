d4f5e629c673850f9ab4747a1d6d6bc89834d6ff
VovaSles <vovaswebdev@gmail.com>
2020-11-22 11:58:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <title>8</title>
+	<link rel="stylesheet" href="mystyle.css">
+</head>
+
+<body>
+    <h1>רשימת קניות</h1>
+    <h2>
+        <a class="lnk-large" href="http://en.wikipedia.org/wiki/Dairy_product">
+            מוצרי חלב בויקיפדיה
+        </a>
+    </h2>
+    <ol>
+        <li>חלב</li>
+        <li class="marked">ביצים</li>
+        <li class="marked">צהובה גבינה</li>
+    </ol>
+    <a class="lnk-large" href="http://market.zap.co.il/default.aspx?siteid=2">
+        זאפ מרקט - השוואת מחירי סופרים
+    </a>
+</body>
+
+</html>
